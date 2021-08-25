d32e05f6e947b15a16fa41748b1a67cf8b37eee6
Graeme Candlish <graemecandlish@gmail.com>
2021-08-24 22:44:15 -0400

    Initial commit

---
+++ programas_chap1/defineGridBlock.cu
@@ -0,0 +1,31 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  // define total data elements
+  int nElem = 1024;
+
+  // define grid and block structure
+  dim3 block (1024);
+  dim3 grid ((nElem+block.x-1)/block.x);
+  printf("grid.x %d block.x %d \n",grid.x, block.x);
+
+  // reset block
+  block.x = 512;
+  grid.x = (nElem+block.x-1)/block.x;
+  printf("grid.x %d block.x %d \n",grid.x, block.x);
+
+  // reset block
+  block.x = 256;
+  grid.x = (nElem+block.x-1)/block.x;
+  printf("grid.x %d block.x %d \n",grid.x, block.x);
+
+  // reset block
+  block.x = 128;
+  grid.x = (nElem+block.x-1)/block.x;
+  printf("grid.x %d block.x %d \n",grid.x, block.x);
+
+  // reset device before you leave
+  cudaDeviceReset();
+  return(0);
+}
